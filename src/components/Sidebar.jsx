3be07ae85aad7348a5d6a931b7bffbd80eac6f37
Dalon <italonso@hotmail.com>
2026-06-01 03:39:01 -0500

    Modificacion de Sidebar para entrar a la nueva sección

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,6 +6,7 @@ function Sidebar({ setPantalla }) {
       </h3>
       <ul className="group-list">
         <li className="group-item" onClick={() => setPantalla('home')}>🏠 Home</li>
+        <li onClick={() => setPantalla('groups')} className="group-item">📚 Grupos Temáticos</li>
         <li className="group-item" onClick={() => setPantalla('live')} style={{ color: '#ef4444', fontWeight: 'bold' }}>🔴 EN VIVO</li>
         <li className="group-item" onClick={() => setPantalla('groups')}>🕹️ r/retrogaming</li>
         <li className="group-item" onClick={() => setPantalla('groups')}>👾 r/HTML</li>
